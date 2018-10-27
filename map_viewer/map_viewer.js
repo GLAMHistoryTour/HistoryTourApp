f88647b7e0c4b56030f22e5b1569ca788e298f4c
Rafael Arizcorreta <macclonker@gmail.com>
2018-10-27 14:59:07 +0200

    fixed location

--- map_viewer/map_viewer.js
+++ map_viewer/map_viewer.js
@@ -1,25 +1,23 @@
 var points = new Array();
-var x;
+var error_msg;
 var map;
 var me;
+var me_radius;
 
 window.onload = function () {
     map = L.map('mapid').setView([47.3772429, 8.531449499999999], 15);
-    navigator.geolocation.watchPosition(function(position){
-        /*if(position != null) {
-            var me = L.circleMarker(position.coords.latitude, position.coords.longitude).addTo(map);
-        }*/
-    });
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
-    x = document.getElementById("demo");
+    error_msg = document.getElementById("no-navi");
     var tour = findGetParameter('json');
     if(tour != null){
         loadTour(tour);   
     }
+
+    startTracking();
     /*L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYXJpdHNjaGNvcmFlIiwiYSI6ImNqbnExMnBodjB1MmMzd3ExZ3pzbmsxemUifQ.RmValv0ybcpFGIm2VpX3Vw', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -28,20 +26,40 @@ window.onload = function () {
     }).addTo(map);*/
 }
 
-function getLocation() {
+function getLocation(){
+
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function startTracking() {
     if (navigator.geolocation) {
-    console.log('getLocation1');
+        error_msg.style.display = "none";
+
+        //on start up
+        navigator.geolocation.getCurrentPosition(function(e){
+            var radius = e.coords.accuracy / 2;
+            var coord = L.latLng(e.coords.latitude, e.coords.longitude);
+            me = L.circleMarker(coord).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+            me_radius =  L.circle(coord, radius).addTo(map);
+            map.panTo(L.latLng(e.coords.latitude, e.coords.longitude));
+        });
+
+        //watcher
         navigator.geolocation.watchPosition(showPosition);
     } else { 
-        console.log('getLocation2');
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        error_msg.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function showPosition(position) {
-    console.log('showPosition ' + position.coords.latitude);
-    map.setView([position.coords.latitude, position.coords.longitude], 15);
-    //me = L.circleMarker(position.coords.latitude, position.coords.longitude).addTo(map);
+
+function showPosition(e) {
+    var radius = e.coords.accuracy / 2;
+    me.setLatLng(L.latLng(e.coords.latitude, e.coords.longitude));
+    me.setPopupContent("You are within " + radius + " meters from this point");
+
+    me_radius.setLatLng(L.latLng(e.coords.latitude, e.coords.longitude));
+    me_radius.setRadius(radius);
+
     //window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]);
 }
 
