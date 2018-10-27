bcfe6912f23ef07c11133d0fd2e9bd5c2a1a58b2
Kenny <kennyfloria@hotmail.com>
2018-10-27 12:45:55 +0200

    Add basic navigation

--- tour_description/description.js
+++ tour_description/description.js
@@ -1,12 +1,26 @@
 window.onload = function () {
     var request = new XMLHttpRequest();
-    request.open("GET", "../config/braun.json", false);
+    var urlParams = new URLSearchParams(window.location.search);
+    request.open("GET", "../config/"+urlParams.get('json'), false);
     request.send(null);
     var tour = JSON.parse(request.responseText);
-    var link = document.createElement('img');
-    link.classList.add("img-fluid");
-    link.src = tour.imageUrl
-    link.innerHTML = tour.title;
-    document.querySelector('#content-column').appendChild(link);
-    
+    var tourTitle = document.createElement('h1');
+    tourTitle.innerHTML = tour.title;
+    var image = document.createElement('img');
+    image.classList.add("img-fluid");
+    image.src = tour.imageUrl
+    image.innerHTML = tour.title;
+    var tourDescription = document.createElement('p');
+    tourDescription.innerHTML = tour.description;
+    document.querySelector('#content-column').appendChild(tourTitle);
+    document.querySelector('#content-column').appendChild(image);
+    document.querySelector('#content-column').appendChild(tourDescription);
+    var startButton = document.createElement('button');
+    startButton.innerHTML = "Start tour";
+    startButton.classList.add("btn");
+    startButton.classList.add("btn-primary");
+    startButton.onclick = function(){
+         window.location = "../map_viewer/map_viewer.html?json="+urlParams.get('json');
+    };
+    document.querySelector('#content-column').appendChild(startButton);
 }
