4e0360a73bc9b41e4831079d5005f831864575f5
Kenny <kennyfloria@hotmail.com>
2018-10-27 19:19:54 +0200

    fix bug in distance code

--- map_viewer/map_viewer.js
+++ map_viewer/map_viewer.js
@@ -72,8 +72,7 @@ function compareDistance(coord) {
     points.forEach(function (element) {
         var dist = coord.distanceTo(element.getLatLng());
         console.log(dist);
-        //TODO get dist from json
-        if (dist < 10) {
+        if (dist < element.requested_radius) {
             found = true;
             element.on('click', function (e) {
                 console.log('whoot');
